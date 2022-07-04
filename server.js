21dfc0acf4f92891076afe5959aab83a948682bc
raisbekk <raisbekk7@gmail.com>
2022-07-04 12:05:38 +0500

    params

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors"
 import dotenv from  "dotenv"
+import authRoutes from "./routes/auth.routes.js"
 
 dotenv.config()
 
@@ -11,7 +12,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 
-
+app.use(authRoutes)
 
 
 
--- service/user.service.js
+++ service/user.service.js
@@ -1,15 +1,20 @@
 import {prisma} from "../config/database.js"
 
 
-export function createUser(username,password){
+export function createUser(username,password,firstName,lastName,age,token){
     return prisma.user.create(
     {data:
     {
         username,
-        password
+        password,
+        firstName,
+        lastName,
+        age,
+        token
     }} 
     )
 }
+
 export function findUser(username){
     return prisma.user.findUnique({
         where:{
